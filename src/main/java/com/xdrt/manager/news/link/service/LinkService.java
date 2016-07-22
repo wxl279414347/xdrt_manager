13fb2be4b1c543fc5c3b4a9fb293aaa70d48f282
wxl <279414347@qq.com>
2016-07-22 17:59:18 +0800

    tijiao

---
+++ src/main/java/com/xdrt/manager/news/link/service/LinkService.java
@@ -0,0 +1,27 @@
+/*
+ * Powered By [Transnal ES Framework]
+ * Web Site: http://www.transnal.com
+ * Copyright (c) 2010 - 2014
+ */
+package com.xdrt.manager.news.link.service;
+
+import com.xdrt.manager.common.inject.annotation.BaseComponent;
+import com.xdrt.manager.common.service.BaseService;
+import com.xdrt.manager.news.link.entity.Link;
+import com.xdrt.manager.news.link.repository.LinkRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/*
+ * User: Transnal Team
+ * Date: 2014-11-25
+ * Version: 1.0
+ */
+@Service
+public class LinkService extends BaseService<Link, String> {
+
+    @Autowired
+    @BaseComponent
+    private LinkRepository linkRepository;
+
+}
